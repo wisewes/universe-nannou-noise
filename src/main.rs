3b25fe399f5fa60cbc1a9e925ffc56cc583a4a6d
Wes Wise <wisewes@gmail.com>
2022-02-05 14:55:05 -0800

    adjustment

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,16 @@ struct Model {
 }
 
 struct Thing {
-    position: Vec2,
+    positions: Vec<Vec2>,
 }
 
 impl Thing {
     pub fn new(p: Vec2) -> Self {
-        Thing { position: p }
+        let mut positions = Vec::new();
+        positions.push(p);
+        Thing {
+            positions
+        }
     }
 }
 
@@ -42,21 +46,31 @@ fn model(app: &App) -> Model {
     }
 }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    let sn = 0.01;
+fn update(app: &App, model: &mut Model, _update: Update) {
+    let time = app.elapsed_frames() as f32 / 120.0;
+    // let sn = 0.01 * time.cos() as f64 * 0.05;
+    let sn = 0.01 * time.tan() as f64 * 0.9;
     for thing in model.things.iter_mut() {
-        thing.position += vec2(            
-            model.noise.get([sn*thing.position.x as f64, sn*thing.position.y as f64, 0.0]) as f32,
-            model.noise.get([sn*thing.position.x as f64, sn*thing.position.y as f64, 1.0]) as f32,
 
-        );
+        thing.positions.clear();
+        thing.positions.push(vec2(
+            (random::<f32>() - 0.5) * 1024.0,
+            (random::<f32>() - 0.5) * 1024.0,
+        ));
+
+        for _k in 0..50 {
+            let last = thing.positions[0];
+            let new = last + vec2(            
+                model.noise.get([sn*last.x as f64, sn*last.y as f64, 0.0]) as f32,
+                model.noise.get([sn*last.x as f64, sn*last.y as f64, 1.0]) as f32,
+            );
+            thing.positions.insert(0, new);
+        }
     }
 }
 
 fn view (app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-
-    let _time = app.elapsed_frames() as f32 / 60.0;
 
     if app.elapsed_frames() == 1 {
         draw.background().color(BLACK);
@@ -73,7 +87,8 @@ fn view (app: &App, model: &Model, frame: Frame) {
     // }
 
     for thing in model.things.iter() {
-        draw.ellipse().xy(thing.position).radius(5.0).color(PURPLE);
+        draw.polyline().points(thing.positions.iter().cloned()).color(PURPLE);
+        // draw.ellipse().xy(thing.position).radius(5.0).color(PURPLE);
     }
 
     draw.to_frame(app, &frame).unwrap();
